0c5ce038ce88e3766f1d897cd981170014e4194b
Kelvin Contreras <kelvincontrera1@gmail.com>
2024-11-12 00:07:33 -0400

    Feat: uploading RESTful API

---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+
+const todos = [
+    { id: 1, text: 'Buy milk', createdAt: new Date() },
+    { id: 2, text: 'Buy bread', createdAt: null},
+    { id: 3, text: 'Buy butter', createdAt: new Date() },
+];
+
+
+export class TodosController {
+
+    //* DI
+    constuctor() {}
+
+    public getTodos = (req: Request, res: Response) => {
+        res.json(todos);
+    }
+
+    public getTodoById = (req: Request, res: Response) => {
+        const id = +req.params.id;
+        if ( isNaN(id) ) res.status(400).json({ error: 'ID argument is not a mumber'})
+        const todo = todos.find( todo => todo.id === id);
+
+        ( todo )
+         ? res.json(todo)
+         : res.status(404).json({error: `TODO WITH ID ${ id } not found`})
+    }
+
+    public createTodo = (req: Request, res: Response) => {
+        const { text } = req.body;
+        if ( !text ) res.status(400).json({ error: 'Text property is requiered '});
+        const newTodo = {
+            id: todos.length + 1,
+            text: text,
+            createdAt: null
+        }
+
+        todos.push(newTodo);
+        
+        res.json( newTodo );
+    }
+
+    public updateTodo =  (req: Request, res: Response ) => {
+        
+        const id = +req.params.id;
+        if ( isNaN(id) ) res.status(400).json({ error: 'ID argument is not a mumber'})
+
+        const todo:any = todos.find( todo => todo.id === id);
+        if ( !todo ) res.status( 404 ).json({ error: `Todo with id ${ id } not found` })
+
+        const { text, createdAt } = req.body;
+
+        todo.text = text || todo.text;
+        ( createdAt === 'null' )
+            ? todo.createdAt = null
+            : todo.createdAt = new Date( createdAt || todo.createdAt )
+        
+        //! OJO referencia
+        // todos.forEach((todo, index) => {
+        //     if ( todo.id === id) {
+        //         todos [index] = todo;
+        //     }
+        // })
+
+        res.json( todo );
+    }
+
+    public deleteTodo = (req: Request, res: Response) => {
+        const id = +req.params.id;
+
+        const todo: any = todos.find( todo => todo.id === id);
+        if( !todo ) res.status(404).json({ error: `Todo with id ${ id } not found`})
+        
+        todos.splice( todos.indexOf(todo), 1);
+
+        res.json( todo );
+    }
+}
